fc68b12b3daed9a6196fdb44b6e0dcc95be74ad9
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-11 01:32:19 -0700

    gawk -i inplace "{ gsub(/ApplicationNamePlaceholder/, \"Intrepion.KlondikeSolitaire\") }; { print }" Intrepion.KlondikeSolitaire.BusinessLogic/Entities/*

--- Intrepion.KlondikeSolitaire.BusinessLogic/Entities/ApplicationUser.cs
+++ Intrepion.KlondikeSolitaire.BusinessLogic/Entities/ApplicationUser.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System.ComponentModel.DataAnnotations;
 
-namespace ApplicationNamePlaceholder.BusinessLogic.Entities;
+namespace Intrepion.KlondikeSolitaire.BusinessLogic.Entities;
 
 public class ApplicationUser : IdentityUser<Guid>
 {
