02329d33dca26d60a0fde35516f23ae6fc51b55c
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-11 01:32:14 -0700

    gawk -i inplace "{ gsub(/ApplicationNamePlaceholder/, \"Intrepion.KlondikeSolitaire\") }; { print }" Intrepion.KlondikeSolitaire.AcceptanceTests/*

--- Intrepion.KlondikeSolitaire.AcceptanceTests/PublicPages.cs
+++ Intrepion.KlondikeSolitaire.AcceptanceTests/PublicPages.cs
@@ -2,7 +2,7 @@
 using Microsoft.Playwright;
 using Microsoft.Playwright.NUnit;
 
-namespace ApplicationNamePlaceholder.AcceptanceTests;
+namespace Intrepion.KlondikeSolitaire.AcceptanceTests;
 
 [Parallelizable(ParallelScope.Self)]
 [TestFixture]
