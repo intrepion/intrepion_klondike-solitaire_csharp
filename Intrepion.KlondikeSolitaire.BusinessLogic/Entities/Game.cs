a682b323680158930299aa36ceef5b2508e9e205
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-11 01:28:15 -0700

    gawk -i inplace "{ gsub(/\/\/ ActualPropertyPlaceholder/, \"public Player? Player { get; set; }\n\ \ \ \ // ActualPropertyPlaceholder\") }; { print }" Intrepion.KlondikeSolitaire.BusinessLogic/Entities/Game.cs

--- Intrepion.KlondikeSolitaire.BusinessLogic/Entities/Game.cs
+++ Intrepion.KlondikeSolitaire.BusinessLogic/Entities/Game.cs
@@ -7,5 +7,6 @@ public class Game
     public ApplicationUser? ApplicationUserUpdatedBy { get; set; }
     public Guid Id { get; set; }
 
+    public Player? Player { get; set; }
     // ActualPropertyPlaceholder
 }
