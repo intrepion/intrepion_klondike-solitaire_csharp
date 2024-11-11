cd7253e6cf77a6da4e1346225e254bb4c3d398db
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-11 01:28:58 -0700

    gawk -i inplace "{ gsub(/EntityNamePlaceholder/, \"Player\") }; { print }" Intrepion.KlondikeSolitaire.BusinessLogic/Grid/Admin/PlayerGrid/PlayerFilterColumns.cs

--- Intrepion.KlondikeSolitaire.BusinessLogic/Grid/Admin/PlayerGrid/PlayerFilterColumns.cs
+++ Intrepion.KlondikeSolitaire.BusinessLogic/Grid/Admin/PlayerGrid/PlayerFilterColumns.cs
@@ -1,6 +1,6 @@
-﻿namespace Intrepion.KlondikeSolitaire.BusinessLogic.Grid.Admin.EntityNamePlaceholderGrid;
+﻿namespace Intrepion.KlondikeSolitaire.BusinessLogic.Grid.Admin.PlayerGrid;
 
-public enum EntityNamePlaceholderFilterColumns
+public enum PlayerFilterColumns
 {
     Id,
 
