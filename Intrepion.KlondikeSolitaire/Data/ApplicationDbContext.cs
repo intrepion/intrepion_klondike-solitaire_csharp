7cec0409c2ae92c8c49ebe43dbcdf1bf14692880
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-11 01:32:13 -0700

    rm -rf Intrepion.KlondikeSolitaire/Data

--- Intrepion.KlondikeSolitaire/Data/ApplicationDbContext.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-
-namespace Intrepion.KlondikeSolitaire.Data;
-
-public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
-{
-}
