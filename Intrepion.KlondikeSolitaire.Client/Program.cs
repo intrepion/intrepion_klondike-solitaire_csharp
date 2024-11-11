b61a363dca370d9d5d5d04ca8ad994444b185428
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-11 01:32:24 -0700

    gawk -i inplace "{ gsub(/ApplicationNamePlaceholder/, \"Intrepion.KlondikeSolitaire\") }; { print }" Intrepion.KlondikeSolitaire.Client/*

--- Intrepion.KlondikeSolitaire.Client/Program.cs
+++ Intrepion.KlondikeSolitaire.Client/Program.cs
@@ -1,4 +1,4 @@
-﻿using ApplicationNamePlaceholder.Client;
+﻿using Intrepion.KlondikeSolitaire.Client;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
