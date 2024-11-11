c43805be7de09897efaf78ff5098cb89b97f2054
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-11 01:29:08 -0700

    gawk -i inplace "{ gsub(/EntityNamePlaceholder/, \"Player\") }; { print }" Intrepion.KlondikeSolitaire.BusinessLogic/Grid/Admin/PlayerGrid/IPlayerFilters.cs

--- Intrepion.KlondikeSolitaire.BusinessLogic/Grid/Admin/PlayerGrid/IPlayerFilters.cs
+++ Intrepion.KlondikeSolitaire.BusinessLogic/Grid/Admin/PlayerGrid/IPlayerFilters.cs
@@ -1,10 +1,10 @@
-﻿namespace Intrepion.KlondikeSolitaire.BusinessLogic.Grid.Admin.EntityNamePlaceholderGrid;
+﻿namespace Intrepion.KlondikeSolitaire.BusinessLogic.Grid.Admin.PlayerGrid;
 
 // Interface for filtering.
-public interface IEntityNamePlaceholderFilters
+public interface IPlayerFilters
 {
-    // The EntityNamePlaceholderFilterColumns being filtered on.
-    EntityNamePlaceholderFilterColumns FilterColumn { get; set; }
+    // The PlayerFilterColumns being filtered on.
+    PlayerFilterColumns FilterColumn { get; set; }
 
     // Loading indicator.
     bool Loading { get; set; }
@@ -18,6 +18,6 @@ public interface IEntityNamePlaceholderFilters
     // Gets or sets a value indicating if the sort is ascending or descending.
     bool SortAscending { get; set; }
 
-    // The EntityNamePlaceholderFilterColumns being sorted.
-    EntityNamePlaceholderFilterColumns SortColumn { get; set; }
+    // The PlayerFilterColumns being sorted.
+    PlayerFilterColumns SortColumn { get; set; }
 }
