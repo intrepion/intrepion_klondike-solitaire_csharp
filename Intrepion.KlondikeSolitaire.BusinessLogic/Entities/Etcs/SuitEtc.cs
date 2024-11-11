70567d08acf56174241b066eb7371445a0f76328
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-11 01:31:54 -0700

    gawk -i inplace "{ gsub(/TableNamePlaceholder/, \"Suits\") }; { print }" Intrepion.KlondikeSolitaire.BusinessLogic/Entities/Etcs/SuitEtc.cs

--- Intrepion.KlondikeSolitaire.BusinessLogic/Entities/Etcs/SuitEtc.cs
+++ Intrepion.KlondikeSolitaire.BusinessLogic/Entities/Etcs/SuitEtc.cs
@@ -7,10 +7,10 @@ public class SuitEtc : IEntityTypeConfiguration<Suit>
 {
     public void Configure(EntityTypeBuilder<Suit> builder)
     {
-        builder.ToTable("TableNamePlaceholder", x => x.IsTemporal());
+        builder.ToTable("Suits", x => x.IsTemporal());
 
         builder.HasOne(x => x.ApplicationUserUpdatedBy)
-            .WithMany(x => x.UpdatedTableNamePlaceholder)
+            .WithMany(x => x.UpdatedSuits)
             .OnDelete(DeleteBehavior.Restrict);
 
         // EntityConfigurationCodePlaceholder
