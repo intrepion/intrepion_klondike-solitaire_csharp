375a97b31f42b3f90032e37874dda682bdb4eb4b
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-11 01:32:22 -0700

    gawk -i inplace "{ gsub(/ApplicationNamePlaceholder/, \"Intrepion.KlondikeSolitaire\") }; { print }" Intrepion.KlondikeSolitaire.BusinessLogic/Grid/Admin/ApplicationRoleGrid/*

--- Intrepion.KlondikeSolitaire.BusinessLogic/Grid/Admin/ApplicationRoleGrid/ApplicationRoleGridQueryAdapter.cs
+++ Intrepion.KlondikeSolitaire.BusinessLogic/Grid/Admin/ApplicationRoleGrid/ApplicationRoleGridQueryAdapter.cs
@@ -1,9 +1,9 @@
 using System.Diagnostics;
 using System.Linq.Expressions;
 using Microsoft.EntityFrameworkCore;
-using ApplicationNamePlaceholder.BusinessLogic.Entities;
+using Intrepion.KlondikeSolitaire.BusinessLogic.Entities;
 
-namespace ApplicationNamePlaceholder.BusinessLogic.Grid.Admin.ApplicationRoleGrid;
+namespace Intrepion.KlondikeSolitaire.BusinessLogic.Grid.Admin.ApplicationRoleGrid;
 
 // Creates the correct expressions to filter and sort.
 public class ApplicationRoleGridQueryAdapter
