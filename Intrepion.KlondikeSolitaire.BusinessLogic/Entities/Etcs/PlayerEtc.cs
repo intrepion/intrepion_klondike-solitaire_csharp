aaf929c58595a597bacba9b7671009025e2f5f3c
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-11 01:29:19 -0700

    gawk -i inplace "{ gsub(/EntityNamePlaceholder/, \"Player\") }; { print }" Intrepion.KlondikeSolitaire.BusinessLogic/Entities/Etcs/PlayerEtc.cs

--- Intrepion.KlondikeSolitaire.BusinessLogic/Entities/Etcs/PlayerEtc.cs
+++ Intrepion.KlondikeSolitaire.BusinessLogic/Entities/Etcs/PlayerEtc.cs
@@ -3,9 +3,9 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace ApplicationNamePlaceholder.BusinessLogic.Entities.Configuration;
 
-public class EntityNamePlaceholderEtc : IEntityTypeConfiguration<EntityNamePlaceholder>
+public class PlayerEtc : IEntityTypeConfiguration<Player>
 {
-    public void Configure(EntityTypeBuilder<EntityNamePlaceholder> builder)
+    public void Configure(EntityTypeBuilder<Player> builder)
     {
         builder.ToTable("TableNamePlaceholder", x => x.IsTemporal());
 
