91ca15b83e4480d9b73be84ffe84516a1f9d214d
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-12 00:04:30 -0700

    gawk -i inplace "{ gsub(/EntityNamePlaceholder/, \"Game\") }; { print }" Intrepion.KlondikeSolitaire.BusinessLogic/Entities/Game.cs

--- Intrepion.KlondikeSolitaire.BusinessLogic/Entities/Game.cs
+++ Intrepion.KlondikeSolitaire.BusinessLogic/Entities/Game.cs
@@ -2,7 +2,7 @@
 
 namespace ApplicationNamePlaceholder.BusinessLogic.Entities;
 
-public class EntityNamePlaceholder
+public class Game
 {
     public ApplicationUser? ApplicationUserUpdatedBy { get; set; }
     public Guid Id { get; set; }
