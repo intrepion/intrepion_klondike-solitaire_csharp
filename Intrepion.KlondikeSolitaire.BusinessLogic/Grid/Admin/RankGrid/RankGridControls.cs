71ed8820026a9ebba71c038ea7aca123e9b4d545
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-12 00:11:23 -0700

    gawk -i inplace "{ gsub(/ApplicationNamePlaceholder/, \"Intrepion.KlondikeSolitaire\") }; { print }" Intrepion.KlondikeSolitaire.BusinessLogic/Grid/Admin/RankGrid/RankGridControls.cs

--- Intrepion.KlondikeSolitaire.BusinessLogic/Grid/Admin/RankGrid/RankGridControls.cs
+++ Intrepion.KlondikeSolitaire.BusinessLogic/Grid/Admin/RankGrid/RankGridControls.cs
@@ -1,4 +1,4 @@
-namespace ApplicationNamePlaceholder.BusinessLogic.Grid.Admin.EntityNamePlaceholderGrid;
+namespace Intrepion.KlondikeSolitaire.BusinessLogic.Grid.Admin.EntityNamePlaceholderGrid;
 
 // State of grid filters.
 public class EntityNamePlaceholderGridControls(IPageHelper pageHelper) : IEntityNamePlaceholderFilters
