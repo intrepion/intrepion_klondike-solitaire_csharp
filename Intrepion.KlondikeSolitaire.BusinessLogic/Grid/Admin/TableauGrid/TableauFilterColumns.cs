5446f71a7a3c916381c5ba584288f47fea396cb2
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-12 00:14:17 -0700

    gawk -i inplace "{ gsub(/EntityNamePlaceholder/, \"Tableau\") }; { print }" Intrepion.KlondikeSolitaire.BusinessLogic/Grid/Admin/TableauGrid/TableauFilterColumns.cs

--- Intrepion.KlondikeSolitaire.BusinessLogic/Grid/Admin/TableauGrid/TableauFilterColumns.cs
+++ Intrepion.KlondikeSolitaire.BusinessLogic/Grid/Admin/TableauGrid/TableauFilterColumns.cs
@@ -1,6 +1,6 @@
-﻿namespace Intrepion.KlondikeSolitaire.BusinessLogic.Grid.Admin.EntityNamePlaceholderGrid;
+﻿namespace Intrepion.KlondikeSolitaire.BusinessLogic.Grid.Admin.TableauGrid;
 
-public enum EntityNamePlaceholderFilterColumns
+public enum TableauFilterColumns
 {
     Id,
 
