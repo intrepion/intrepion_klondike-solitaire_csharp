bb2cac29b04e37d255edec74eded63929c22ea3f
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-12 00:03:13 -0700

    gawk -i inplace "{ gsub(/\/\/ DbSetCodePlaceholder/, \"public DbSet<Foundation> Foundations { get; set; }\n\ \ \ \ // DbSetCodePlaceholder\") }; { print }" Intrepion.KlondikeSolitaire.BusinessLogic/Data/ApplicationDbContext.cs

--- Intrepion.KlondikeSolitaire.BusinessLogic/Data/ApplicationDbContext.cs
+++ Intrepion.KlondikeSolitaire.BusinessLogic/Data/ApplicationDbContext.cs
@@ -12,6 +12,7 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
     public DbSet<CardStock> CardStocks { get; set; }
     public DbSet<CardTableau> CardTableaus { get; set; }
     public DbSet<CardWaste> CardWastes { get; set; }
+    public DbSet<Foundation> Foundations { get; set; }
     // DbSetCodePlaceholder
 
     protected override void OnModelCreating(ModelBuilder builder)
