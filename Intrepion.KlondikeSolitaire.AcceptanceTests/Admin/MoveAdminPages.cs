07dd97f926c2e3e3c12bed19c265c295c5701437
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-12 00:06:31 -0700

    gawk -i inplace "{ gsub(/\/\/ ModifyPropertyCodePlaceholder/, \"await Page.GetByLabel(\\\"From Pile Index:\\\", new() { Exact = true }).FillAsync(\\\"2\\\");\n\ \ \ \ \ \ \ \ // ModifyPropertyCodePlaceholder\") }; { print }" Intrepion.KlondikeSolitaire.AcceptanceTests/Admin/MoveAdminPages.cs

--- Intrepion.KlondikeSolitaire.AcceptanceTests/Admin/MoveAdminPages.cs
+++ Intrepion.KlondikeSolitaire.AcceptanceTests/Admin/MoveAdminPages.cs
@@ -27,6 +27,7 @@ public partial class MoveAdminPages : PageTest
         await Page.GetByRole(AriaRole.Link, new() { Name = "Edit" }).ClickAsync();
         await Expect(Page).ToHaveTitleAsync("Move Edit");
 
+        await Page.GetByLabel("From Pile Index:", new() { Exact = true }).FillAsync("2");
         // ModifyPropertyCodePlaceholder
 
         await Page.GetByRole(AriaRole.Button, new() { Name = "Submit" }).ClickAsync();
