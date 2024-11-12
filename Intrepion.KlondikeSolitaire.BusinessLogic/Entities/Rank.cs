a355f488db06f5ca9d0e69dbeb44d8d0443c68dd
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-12 00:11:41 -0700

    gawk -i inplace "{ gsub(/\/\/ ActualPropertyPlaceholder/, \"public ICollection<Card> Cards { get; set; } = [];\n\ \ \ \ // ActualPropertyPlaceholder\") }; { print }" Intrepion.KlondikeSolitaire.BusinessLogic/Entities/Rank.cs

--- Intrepion.KlondikeSolitaire.BusinessLogic/Entities/Rank.cs
+++ Intrepion.KlondikeSolitaire.BusinessLogic/Entities/Rank.cs
@@ -7,5 +7,6 @@ public class Rank
     public ApplicationUser? ApplicationUserUpdatedBy { get; set; }
     public Guid Id { get; set; }
 
+    public ICollection<Card> Cards { get; set; } = [];
     // ActualPropertyPlaceholder
 }
