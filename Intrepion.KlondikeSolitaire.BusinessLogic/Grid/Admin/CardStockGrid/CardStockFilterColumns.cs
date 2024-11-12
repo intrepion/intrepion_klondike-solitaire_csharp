46d688de41c06e8e3fbfed29e738d7ad59c86d25
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-11 23:59:13 -0700

    gawk -i inplace "{ gsub(/EntityNamePlaceholder/, \"CardStock\") }; { print }" Intrepion.KlondikeSolitaire.BusinessLogic/Grid/Admin/CardStockGrid/CardStockFilterColumns.cs

--- Intrepion.KlondikeSolitaire.BusinessLogic/Grid/Admin/CardStockGrid/CardStockFilterColumns.cs
+++ Intrepion.KlondikeSolitaire.BusinessLogic/Grid/Admin/CardStockGrid/CardStockFilterColumns.cs
@@ -1,6 +1,6 @@
-﻿namespace Intrepion.KlondikeSolitaire.BusinessLogic.Grid.Admin.EntityNamePlaceholderGrid;
+﻿namespace Intrepion.KlondikeSolitaire.BusinessLogic.Grid.Admin.CardStockGrid;
 
-public enum EntityNamePlaceholderFilterColumns
+public enum CardStockFilterColumns
 {
     Id,
 
