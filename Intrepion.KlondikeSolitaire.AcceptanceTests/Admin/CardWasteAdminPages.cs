1d2c71eef2ea3d863b6c5a26e3abfb2a729db7cf
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-12 00:02:01 -0700

    gawk -i inplace "{ gsub(/LowercaseNamePlaceholder/, \"cardWaste\") }; { print }" Intrepion.KlondikeSolitaire.AcceptanceTests/Admin/CardWasteAdminPages.cs

--- Intrepion.KlondikeSolitaire.AcceptanceTests/Admin/CardWasteAdminPages.cs
+++ Intrepion.KlondikeSolitaire.AcceptanceTests/Admin/CardWasteAdminPages.cs
@@ -14,7 +14,7 @@ public partial class CardWasteAdminPages : PageTest
         var faker = new Faker();
         var aRandomString = faker.Random.String2(10);
         var someRandomString = faker.Random.String2(10);
-        await Page.GetByTestId("LowercaseNamePlaceholderNavLink").ClickAsync();
+        await Page.GetByTestId("cardWasteNavLink").ClickAsync();
         await Expect(Page).ToHaveTitleAsync("Card Waste Home");
         await Page.GetByRole(AriaRole.Link, new() { Name = "Create New" }).ClickAsync();
         await Expect(Page).ToHaveTitleAsync("Card Waste Add");
