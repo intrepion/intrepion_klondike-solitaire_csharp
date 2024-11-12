3410cf64b21371c1e6888be45dde948f686ee4df
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-12 00:12:17 -0700

    gawk -i inplace "{ gsub(/EntityNamePlaceholder/, \"Status\") }; { print }" Intrepion.KlondikeSolitaire.BusinessLogic/Grid/Admin/StatusGrid/IStatusFilters.cs

--- Intrepion.KlondikeSolitaire.BusinessLogic/Grid/Admin/StatusGrid/IStatusFilters.cs
+++ Intrepion.KlondikeSolitaire.BusinessLogic/Grid/Admin/StatusGrid/IStatusFilters.cs
@@ -1,10 +1,10 @@
-﻿namespace Intrepion.KlondikeSolitaire.BusinessLogic.Grid.Admin.EntityNamePlaceholderGrid;
+﻿namespace Intrepion.KlondikeSolitaire.BusinessLogic.Grid.Admin.StatusGrid;
 
 // Interface for filtering.
-public interface IEntityNamePlaceholderFilters
+public interface IStatusFilters
 {
-    // The EntityNamePlaceholderFilterColumns being filtered on.
-    EntityNamePlaceholderFilterColumns FilterColumn { get; set; }
+    // The StatusFilterColumns being filtered on.
+    StatusFilterColumns FilterColumn { get; set; }
 
     // Loading indicator.
     bool Loading { get; set; }
@@ -18,6 +18,6 @@ public interface IEntityNamePlaceholderFilters
     // Gets or sets a value indicating if the sort is ascending or descending.
     bool SortAscending { get; set; }
 
-    // The EntityNamePlaceholderFilterColumns being sorted.
-    EntityNamePlaceholderFilterColumns SortColumn { get; set; }
+    // The StatusFilterColumns being sorted.
+    StatusFilterColumns SortColumn { get; set; }
 }
