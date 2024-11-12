0bbfd5668264eea8cef7a36d6eb48de48aee0cb2
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-12 00:07:46 -0700

    gawk -i inplace "{ gsub(/LowercaseNamePlaceholder/, \"pileType\") }; { print }" Intrepion.KlondikeSolitaire.AcceptanceTests/Admin/PileTypeAdminPages.cs

--- Intrepion.KlondikeSolitaire.AcceptanceTests/Admin/PileTypeAdminPages.cs
+++ Intrepion.KlondikeSolitaire.AcceptanceTests/Admin/PileTypeAdminPages.cs
@@ -14,7 +14,7 @@ public partial class PileTypeAdminPages : PageTest
         var faker = new Faker();
         var aRandomString = faker.Random.String2(10);
         var someRandomString = faker.Random.String2(10);
-        await Page.GetByTestId("LowercaseNamePlaceholderNavLink").ClickAsync();
+        await Page.GetByTestId("pileTypeNavLink").ClickAsync();
         await Expect(Page).ToHaveTitleAsync("Pile Type Home");
         await Page.GetByRole(AriaRole.Link, new() { Name = "Create New" }).ClickAsync();
         await Expect(Page).ToHaveTitleAsync("Pile Type Add");
