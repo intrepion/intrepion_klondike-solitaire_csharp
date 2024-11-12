b2a43b1b37cd2164697eb7ce2ae43831f5a671ab
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-12 00:14:43 -0700

    gawk -i inplace "{ gsub(/\/\/ CreatePropertyCodePlaceholder/, \"await Page.GetByLabel(\\\"Pile Index:\\\", new() { Exact = true }).FillAsync(\\\"1\\\");\n\ \ \ \ \ \ \ \ // CreatePropertyCodePlaceholder\") }; { print }" Intrepion.KlondikeSolitaire.AcceptanceTests/Admin/TableauAdminPages.cs

--- Intrepion.KlondikeSolitaire.AcceptanceTests/Admin/TableauAdminPages.cs
+++ Intrepion.KlondikeSolitaire.AcceptanceTests/Admin/TableauAdminPages.cs
@@ -19,6 +19,7 @@ public partial class TableauAdminPages : PageTest
         await Page.GetByRole(AriaRole.Link, new() { Name = "Create New" }).ClickAsync();
         await Expect(Page).ToHaveTitleAsync("Tableau Add");
 
+        await Page.GetByLabel("Pile Index:", new() { Exact = true }).FillAsync("1");
         // CreatePropertyCodePlaceholder
 
         await Page.GetByRole(AriaRole.Button, new() { Name = "Submit" }).ClickAsync();
