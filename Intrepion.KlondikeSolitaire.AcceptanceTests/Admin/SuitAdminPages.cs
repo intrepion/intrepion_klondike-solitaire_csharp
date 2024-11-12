9e6d343fc360cfebe064b606cad60b559d5be833
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-12 00:13:29 -0700

    gawk -i inplace "{ gsub(/\/\/ CreatePropertyCodePlaceholder/, \"await Page.GetByLabel(\\\"Code:\\\", new() { Exact = true }).FillAsync(\\\"aCode\\\" + aRandomString);\n\ \ \ \ \ \ \ \ // CreatePropertyCodePlaceholder\") }; { print }" Intrepion.KlondikeSolitaire.AcceptanceTests/Admin/SuitAdminPages.cs

--- Intrepion.KlondikeSolitaire.AcceptanceTests/Admin/SuitAdminPages.cs
+++ Intrepion.KlondikeSolitaire.AcceptanceTests/Admin/SuitAdminPages.cs
@@ -19,6 +19,7 @@ public partial class SuitAdminPages : PageTest
         await Page.GetByRole(AriaRole.Link, new() { Name = "Create New" }).ClickAsync();
         await Expect(Page).ToHaveTitleAsync("Suit Add");
 
+        await Page.GetByLabel("Code:", new() { Exact = true }).FillAsync("aCode" + aRandomString);
         // CreatePropertyCodePlaceholder
 
         await Page.GetByRole(AriaRole.Button, new() { Name = "Submit" }).ClickAsync();
