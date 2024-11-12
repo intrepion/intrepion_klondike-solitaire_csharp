1865afc0a3ce9a514df4b48cbd601e598999060f
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-11 23:59:24 -0700

    gawk -i inplace "{ gsub(/LowercaseNamePlaceholder/, \"cardStock\") }; { print }" Intrepion.KlondikeSolitaire.AcceptanceTests/Admin/CardStockAdminPages.cs

--- Intrepion.KlondikeSolitaire.AcceptanceTests/Admin/CardStockAdminPages.cs
+++ Intrepion.KlondikeSolitaire.AcceptanceTests/Admin/CardStockAdminPages.cs
@@ -14,7 +14,7 @@ public partial class CardStockAdminPages : PageTest
         var faker = new Faker();
         var aRandomString = faker.Random.String2(10);
         var someRandomString = faker.Random.String2(10);
-        await Page.GetByTestId("LowercaseNamePlaceholderNavLink").ClickAsync();
+        await Page.GetByTestId("cardStockNavLink").ClickAsync();
         await Expect(Page).ToHaveTitleAsync("Card Stock Home");
         await Page.GetByRole(AriaRole.Link, new() { Name = "Create New" }).ClickAsync();
         await Expect(Page).ToHaveTitleAsync("Card Stock Add");
