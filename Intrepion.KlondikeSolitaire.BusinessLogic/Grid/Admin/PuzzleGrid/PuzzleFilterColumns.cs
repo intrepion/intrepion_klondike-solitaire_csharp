e5796b584152a215001d3eb321a0244af7668aa2
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-12 00:09:38 -0700

    gawk -i inplace "{ gsub(/EntityNamePlaceholder/, \"Puzzle\") }; { print }" Intrepion.KlondikeSolitaire.BusinessLogic/Grid/Admin/PuzzleGrid/PuzzleFilterColumns.cs

--- Intrepion.KlondikeSolitaire.BusinessLogic/Grid/Admin/PuzzleGrid/PuzzleFilterColumns.cs
+++ Intrepion.KlondikeSolitaire.BusinessLogic/Grid/Admin/PuzzleGrid/PuzzleFilterColumns.cs
@@ -1,6 +1,6 @@
-﻿namespace Intrepion.KlondikeSolitaire.BusinessLogic.Grid.Admin.EntityNamePlaceholderGrid;
+﻿namespace Intrepion.KlondikeSolitaire.BusinessLogic.Grid.Admin.PuzzleGrid;
 
-public enum EntityNamePlaceholderFilterColumns
+public enum PuzzleFilterColumns
 {
     Id,
 
