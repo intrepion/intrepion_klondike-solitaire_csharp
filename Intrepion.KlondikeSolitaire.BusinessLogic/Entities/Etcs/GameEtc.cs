619520ed54585236a327c926f939bdd905920e61
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-12 00:05:06 -0700

    gawk -i inplace "{ gsub(/\/\/ EntityConfigurationCodePlaceholder/, \"builder.HasOne(x => x.StatusId)\n\ \ \ \ \ \ \ \ \ \ \ \ .WithMany(x => x.Games)\n\ \ \ \ \ \ \ \ \ \ \ \ .OnDelete(DeleteBehavior.Restrict);\n\ \ \ \ \ \ \ \ // EntityConfigurationCodePlaceholder\") }; { print }" Intrepion.KlondikeSolitaire.BusinessLogic/Entities/Etcs/GameEtc.cs

--- Intrepion.KlondikeSolitaire.BusinessLogic/Entities/Etcs/GameEtc.cs
+++ Intrepion.KlondikeSolitaire.BusinessLogic/Entities/Etcs/GameEtc.cs
@@ -19,6 +19,9 @@ public class GameEtc : IEntityTypeConfiguration<Game>
         builder.HasOne(x => x.PuzzleId)
             .WithMany(x => x.Games)
             .OnDelete(DeleteBehavior.Restrict);
+        builder.HasOne(x => x.StatusId)
+            .WithMany(x => x.Games)
+            .OnDelete(DeleteBehavior.Restrict);
         // EntityConfigurationCodePlaceholder
     }
 }
