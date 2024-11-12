dc564a4831a1c5892ee737153077433fa348b97c
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-12 00:06:13 -0700

    gawk -i inplace "{ gsub(/\/\/ ActualPropertyPlaceholder/, \"public Card? Card1Id { get; set; }\n\ \ \ \ // ActualPropertyPlaceholder\") }; { print }" Intrepion.KlondikeSolitaire.BusinessLogic/Entities/Move.cs

--- Intrepion.KlondikeSolitaire.BusinessLogic/Entities/Move.cs
+++ Intrepion.KlondikeSolitaire.BusinessLogic/Entities/Move.cs
@@ -7,5 +7,6 @@ public class Move
     public ApplicationUser? ApplicationUserUpdatedBy { get; set; }
     public Guid Id { get; set; }
 
+    public Card? Card1Id { get; set; }
     // ActualPropertyPlaceholder
 }
