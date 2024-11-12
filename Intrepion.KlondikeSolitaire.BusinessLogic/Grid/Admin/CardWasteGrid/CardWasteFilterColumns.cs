18b5ba5549c8a85cbf9bfdbbe96eed7db9cb2401
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-12 00:01:51 -0700

    gawk -i inplace "{ gsub(/EntityNamePlaceholder/, \"CardWaste\") }; { print }" Intrepion.KlondikeSolitaire.BusinessLogic/Grid/Admin/CardWasteGrid/CardWasteFilterColumns.cs

--- Intrepion.KlondikeSolitaire.BusinessLogic/Grid/Admin/CardWasteGrid/CardWasteFilterColumns.cs
+++ Intrepion.KlondikeSolitaire.BusinessLogic/Grid/Admin/CardWasteGrid/CardWasteFilterColumns.cs
@@ -1,6 +1,6 @@
-﻿namespace Intrepion.KlondikeSolitaire.BusinessLogic.Grid.Admin.EntityNamePlaceholderGrid;
+﻿namespace Intrepion.KlondikeSolitaire.BusinessLogic.Grid.Admin.CardWasteGrid;
 
-public enum EntityNamePlaceholderFilterColumns
+public enum CardWasteFilterColumns
 {
     Id,
 
