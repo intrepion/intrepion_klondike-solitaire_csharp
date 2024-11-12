5b2bef23e1a5587acd27ea96d3d2f8640379cd24
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-11 23:58:13 -0700

    gawk -i inplace "{ gsub(/LowercaseNamePlaceholder/, \"cardFoundation\") }; { print }" Intrepion.KlondikeSolitaire.AcceptanceTests/Admin/CardFoundationAdminPages.cs

--- Intrepion.KlondikeSolitaire.AcceptanceTests/Admin/CardFoundationAdminPages.cs
+++ Intrepion.KlondikeSolitaire.AcceptanceTests/Admin/CardFoundationAdminPages.cs
@@ -14,7 +14,7 @@ public partial class CardFoundationAdminPages : PageTest
         var faker = new Faker();
         var aRandomString = faker.Random.String2(10);
         var someRandomString = faker.Random.String2(10);
-        await Page.GetByTestId("LowercaseNamePlaceholderNavLink").ClickAsync();
+        await Page.GetByTestId("cardFoundationNavLink").ClickAsync();
         await Expect(Page).ToHaveTitleAsync("Card Foundation Home");
         await Page.GetByRole(AriaRole.Link, new() { Name = "Create New" }).ClickAsync();
         await Expect(Page).ToHaveTitleAsync("Card Foundation Add");
