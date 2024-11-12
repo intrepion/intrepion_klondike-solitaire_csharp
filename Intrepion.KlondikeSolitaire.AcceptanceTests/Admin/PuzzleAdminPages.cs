a924a69b4a765a02cbe74635f3c4ab6cf7bc839a
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-12 00:09:48 -0700

    gawk -i inplace "{ gsub(/LowercaseNamePlaceholder/, \"puzzle\") }; { print }" Intrepion.KlondikeSolitaire.AcceptanceTests/Admin/PuzzleAdminPages.cs

--- Intrepion.KlondikeSolitaire.AcceptanceTests/Admin/PuzzleAdminPages.cs
+++ Intrepion.KlondikeSolitaire.AcceptanceTests/Admin/PuzzleAdminPages.cs
@@ -14,7 +14,7 @@ public partial class PuzzleAdminPages : PageTest
         var faker = new Faker();
         var aRandomString = faker.Random.String2(10);
         var someRandomString = faker.Random.String2(10);
-        await Page.GetByTestId("LowercaseNamePlaceholderNavLink").ClickAsync();
+        await Page.GetByTestId("puzzleNavLink").ClickAsync();
         await Expect(Page).ToHaveTitleAsync("Puzzle Home");
         await Page.GetByRole(AriaRole.Link, new() { Name = "Create New" }).ClickAsync();
         await Expect(Page).ToHaveTitleAsync("Puzzle Add");
