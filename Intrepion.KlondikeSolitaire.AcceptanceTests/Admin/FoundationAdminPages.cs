e8736e08c51196c6bc047457c8a7396b64280443
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-12 00:03:12 -0700

    gawk -i inplace "{ gsub(/LowercaseNamePlaceholder/, \"foundation\") }; { print }" Intrepion.KlondikeSolitaire.AcceptanceTests/Admin/FoundationAdminPages.cs

--- Intrepion.KlondikeSolitaire.AcceptanceTests/Admin/FoundationAdminPages.cs
+++ Intrepion.KlondikeSolitaire.AcceptanceTests/Admin/FoundationAdminPages.cs
@@ -14,7 +14,7 @@ public partial class FoundationAdminPages : PageTest
         var faker = new Faker();
         var aRandomString = faker.Random.String2(10);
         var someRandomString = faker.Random.String2(10);
-        await Page.GetByTestId("LowercaseNamePlaceholderNavLink").ClickAsync();
+        await Page.GetByTestId("foundationNavLink").ClickAsync();
         await Expect(Page).ToHaveTitleAsync("Foundation Home");
         await Page.GetByRole(AriaRole.Link, new() { Name = "Create New" }).ClickAsync();
         await Expect(Page).ToHaveTitleAsync("Foundation Add");
