19f6efd361e18f251a60750375128aa5d6220d78
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-12 00:03:37 -0700

    gawk -i inplace "{ gsub(/\/\/ ActualPropertyPlaceholder/, \"public Puzzle? PuzzleId { get; set; }\n\ \ \ \ // ActualPropertyPlaceholder\") }; { print }" Intrepion.KlondikeSolitaire.BusinessLogic/Entities/Foundation.cs

--- Intrepion.KlondikeSolitaire.BusinessLogic/Entities/Foundation.cs
+++ Intrepion.KlondikeSolitaire.BusinessLogic/Entities/Foundation.cs
@@ -9,5 +9,6 @@ public class Foundation
 
     public Game? GameId { get; set; }
     public int PileIndex { get; set; }
+    public Puzzle? PuzzleId { get; set; }
     // ActualPropertyPlaceholder
 }
