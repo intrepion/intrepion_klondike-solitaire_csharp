39a1709a1e0407b300b739c98f9f2d155269e312
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-12 00:04:20 -0700

    gawk -i inplace "{ gsub(/EntityNamePlaceholder/, \"Game\") }; { print }" Intrepion.KlondikeSolitaire.BusinessLogic/Grid/Admin/GameGrid/GameGridControls.cs

--- Intrepion.KlondikeSolitaire.BusinessLogic/Grid/Admin/GameGrid/GameGridControls.cs
+++ Intrepion.KlondikeSolitaire.BusinessLogic/Grid/Admin/GameGrid/GameGridControls.cs
@@ -1,7 +1,7 @@
-namespace Intrepion.KlondikeSolitaire.BusinessLogic.Grid.Admin.EntityNamePlaceholderGrid;
+namespace Intrepion.KlondikeSolitaire.BusinessLogic.Grid.Admin.GameGrid;
 
 // State of grid filters.
-public class EntityNamePlaceholderGridControls(IPageHelper pageHelper) : IEntityNamePlaceholderFilters
+public class GameGridControls(IPageHelper pageHelper) : IGameFilters
 {
     // Keep state of paging.
     public IPageHelper PageHelper { get; set; } = pageHelper;
@@ -10,13 +10,13 @@ public class EntityNamePlaceholderGridControls(IPageHelper pageHelper) : IEntity
     public bool Loading { get; set; }
 
     // Column to sort by.
-    public EntityNamePlaceholderFilterColumns SortColumn { get; set; } = EntityNamePlaceholderFilterColumns.Id;
+    public GameFilterColumns SortColumn { get; set; } = GameFilterColumns.Id;
 
     // True when sorting ascending, otherwise sort descending.
     public bool SortAscending { get; set; } = true;
 
     // Column filtered text is against.
-    public EntityNamePlaceholderFilterColumns FilterColumn { get; set; } = EntityNamePlaceholderFilterColumns.Id;
+    public GameFilterColumns FilterColumn { get; set; } = GameFilterColumns.Id;
 
     // Text to filter on.
     public string? FilterText { get; set; }
