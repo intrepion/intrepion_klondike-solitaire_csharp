91d9200d80318b920ff59ef7b24dbd8613f43f86
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-12 00:06:03 -0700

    gawk -i inplace "{ gsub(/LowercaseNamePlaceholder/, \"move\") }; { print }" Intrepion.KlondikeSolitaire.AcceptanceTests/Admin/MoveAdminPages.cs

--- Intrepion.KlondikeSolitaire.AcceptanceTests/Admin/MoveAdminPages.cs
+++ Intrepion.KlondikeSolitaire.AcceptanceTests/Admin/MoveAdminPages.cs
@@ -14,7 +14,7 @@ public partial class MoveAdminPages : PageTest
         var faker = new Faker();
         var aRandomString = faker.Random.String2(10);
         var someRandomString = faker.Random.String2(10);
-        await Page.GetByTestId("LowercaseNamePlaceholderNavLink").ClickAsync();
+        await Page.GetByTestId("moveNavLink").ClickAsync();
         await Expect(Page).ToHaveTitleAsync("Move Home");
         await Page.GetByRole(AriaRole.Link, new() { Name = "Create New" }).ClickAsync();
         await Expect(Page).ToHaveTitleAsync("Move Add");
