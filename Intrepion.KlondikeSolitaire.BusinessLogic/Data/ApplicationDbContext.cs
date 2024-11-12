6ae6e01006c7b8f11c0969d5c5e7500fcb5abf15
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-12 00:15:06 -0700

    gawk -i inplace "{ gsub(/ApplicationNamePlaceholder/, \"Intrepion.KlondikeSolitaire\") }; { print }" Intrepion.KlondikeSolitaire.BusinessLogic/Data/*

--- Intrepion.KlondikeSolitaire.BusinessLogic/Data/ApplicationDbContext.cs
+++ Intrepion.KlondikeSolitaire.BusinessLogic/Data/ApplicationDbContext.cs
@@ -1,9 +1,9 @@
-﻿using ApplicationNamePlaceholder.BusinessLogic.Entities;
-using ApplicationNamePlaceholder.BusinessLogic.Entities.Configuration;
+﻿using Intrepion.KlondikeSolitaire.BusinessLogic.Entities;
+using Intrepion.KlondikeSolitaire.BusinessLogic.Entities.Configuration;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace ApplicationNamePlaceholder.BusinessLogic.Data;
+namespace Intrepion.KlondikeSolitaire.BusinessLogic.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser, ApplicationRole, Guid, ApplicationUserClaim, ApplicationUserRole, ApplicationUserLogin, ApplicationRoleClaim, ApplicationUserToken>(options)
 {
