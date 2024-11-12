244a42d222e2f7eddfcfb02d822f8a792e326184
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-12 00:00:43 -0700

    gawk -i inplace "{ gsub(/EntityNamePlaceholder/, \"CardTableau\") }; { print }" Intrepion.KlondikeSolitaire.BusinessLogic/Grid/Admin/CardTableauGrid/CardTableauGridControls.cs

--- Intrepion.KlondikeSolitaire.BusinessLogic/Grid/Admin/CardTableauGrid/CardTableauGridControls.cs
+++ Intrepion.KlondikeSolitaire.BusinessLogic/Grid/Admin/CardTableauGrid/CardTableauGridControls.cs
@@ -1,7 +1,7 @@
-namespace Intrepion.KlondikeSolitaire.BusinessLogic.Grid.Admin.EntityNamePlaceholderGrid;
+namespace Intrepion.KlondikeSolitaire.BusinessLogic.Grid.Admin.CardTableauGrid;
 
 // State of grid filters.
-public class EntityNamePlaceholderGridControls(IPageHelper pageHelper) : IEntityNamePlaceholderFilters
+public class CardTableauGridControls(IPageHelper pageHelper) : ICardTableauFilters
 {
     // Keep state of paging.
     public IPageHelper PageHelper { get; set; } = pageHelper;
@@ -10,13 +10,13 @@ public class EntityNamePlaceholderGridControls(IPageHelper pageHelper) : IEntity
     public bool Loading { get; set; }
 
     // Column to sort by.
-    public EntityNamePlaceholderFilterColumns SortColumn { get; set; } = EntityNamePlaceholderFilterColumns.Id;
+    public CardTableauFilterColumns SortColumn { get; set; } = CardTableauFilterColumns.Id;
 
     // True when sorting ascending, otherwise sort descending.
     public bool SortAscending { get; set; } = true;
 
     // Column filtered text is against.
-    public EntityNamePlaceholderFilterColumns FilterColumn { get; set; } = EntityNamePlaceholderFilterColumns.Id;
+    public CardTableauFilterColumns FilterColumn { get; set; } = CardTableauFilterColumns.Id;
 
     // Text to filter on.
     public string? FilterText { get; set; }
