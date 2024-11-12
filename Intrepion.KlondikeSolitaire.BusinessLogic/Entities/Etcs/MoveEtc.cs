5901643f3c25735478791bbbe4b81f10ffc0b411
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-12 00:06:08 -0700

    gawk -i inplace "{ gsub(/EntityNamePlaceholder/, \"Move\") }; { print }" Intrepion.KlondikeSolitaire.BusinessLogic/Entities/Etcs/MoveEtc.cs

--- Intrepion.KlondikeSolitaire.BusinessLogic/Entities/Etcs/MoveEtc.cs
+++ Intrepion.KlondikeSolitaire.BusinessLogic/Entities/Etcs/MoveEtc.cs
@@ -3,9 +3,9 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace ApplicationNamePlaceholder.BusinessLogic.Entities.Configuration;
 
-public class EntityNamePlaceholderEtc : IEntityTypeConfiguration<EntityNamePlaceholder>
+public class MoveEtc : IEntityTypeConfiguration<Move>
 {
-    public void Configure(EntityTypeBuilder<EntityNamePlaceholder> builder)
+    public void Configure(EntityTypeBuilder<Move> builder)
     {
         builder.ToTable("TableNamePlaceholder", x => x.IsTemporal());
 
