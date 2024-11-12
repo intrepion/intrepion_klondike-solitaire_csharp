f3fad073d7393c1bcad29edf1ecbbf642f0f5793
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-12 00:07:51 -0700

    gawk -i inplace "{ gsub(/EntityNamePlaceholder/, \"PileType\") }; { print }" Intrepion.KlondikeSolitaire.BusinessLogic/Entities/Etcs/PileTypeEtc.cs

--- Intrepion.KlondikeSolitaire.BusinessLogic/Entities/Etcs/PileTypeEtc.cs
+++ Intrepion.KlondikeSolitaire.BusinessLogic/Entities/Etcs/PileTypeEtc.cs
@@ -3,9 +3,9 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace ApplicationNamePlaceholder.BusinessLogic.Entities.Configuration;
 
-public class EntityNamePlaceholderEtc : IEntityTypeConfiguration<EntityNamePlaceholder>
+public class PileTypeEtc : IEntityTypeConfiguration<PileType>
 {
-    public void Configure(EntityTypeBuilder<EntityNamePlaceholder> builder)
+    public void Configure(EntityTypeBuilder<PileType> builder)
     {
         builder.ToTable("TableNamePlaceholder", x => x.IsTemporal());
 
