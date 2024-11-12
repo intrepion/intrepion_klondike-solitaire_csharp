f83cd32c987575ec3ee95f00b3b5737c035b7e9a
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-12 00:06:08 -0700

    gawk -i inplace "{ gsub(/TableNamePlaceholder/, \"Moves\") }; { print }" Intrepion.KlondikeSolitaire.BusinessLogic/Entities/Etcs/MoveEtc.cs

--- Intrepion.KlondikeSolitaire.BusinessLogic/Entities/Etcs/MoveEtc.cs
+++ Intrepion.KlondikeSolitaire.BusinessLogic/Entities/Etcs/MoveEtc.cs
@@ -7,10 +7,10 @@ public class MoveEtc : IEntityTypeConfiguration<Move>
 {
     public void Configure(EntityTypeBuilder<Move> builder)
     {
-        builder.ToTable("TableNamePlaceholder", x => x.IsTemporal());
+        builder.ToTable("Moves", x => x.IsTemporal());
 
         builder.HasOne(x => x.ApplicationUserUpdatedBy)
-            .WithMany(x => x.UpdatedTableNamePlaceholder)
+            .WithMany(x => x.UpdatedMoves)
             .OnDelete(DeleteBehavior.Restrict);
 
         // EntityConfigurationCodePlaceholder
