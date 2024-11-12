1dd27b0b55f9150dc4371e88cfcfd4fb0979f233
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-12 00:12:20 -0700

    gawk -i inplace "{ gsub(/LowercaseNamePlaceholder/, \"status\") }; { print }" Intrepion.KlondikeSolitaire.AcceptanceTests/Admin/StatusAdminPages.cs

--- Intrepion.KlondikeSolitaire.AcceptanceTests/Admin/StatusAdminPages.cs
+++ Intrepion.KlondikeSolitaire.AcceptanceTests/Admin/StatusAdminPages.cs
@@ -14,7 +14,7 @@ public partial class StatusAdminPages : PageTest
         var faker = new Faker();
         var aRandomString = faker.Random.String2(10);
         var someRandomString = faker.Random.String2(10);
-        await Page.GetByTestId("LowercaseNamePlaceholderNavLink").ClickAsync();
+        await Page.GetByTestId("statusNavLink").ClickAsync();
         await Expect(Page).ToHaveTitleAsync("Status Home");
         await Page.GetByRole(AriaRole.Link, new() { Name = "Create New" }).ClickAsync();
         await Expect(Page).ToHaveTitleAsync("Status Add");
