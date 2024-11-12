aa857aaa5ac6e9f6cef560336ae3083d6be29f95
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-12 00:14:26 -0700

    gawk -i inplace "{ gsub(/LowercaseNamePlaceholder/, \"tableau\") }; { print }" Intrepion.KlondikeSolitaire.AcceptanceTests/Admin/TableauAdminPages.cs

--- Intrepion.KlondikeSolitaire.AcceptanceTests/Admin/TableauAdminPages.cs
+++ Intrepion.KlondikeSolitaire.AcceptanceTests/Admin/TableauAdminPages.cs
@@ -14,7 +14,7 @@ public partial class TableauAdminPages : PageTest
         var faker = new Faker();
         var aRandomString = faker.Random.String2(10);
         var someRandomString = faker.Random.String2(10);
-        await Page.GetByTestId("LowercaseNamePlaceholderNavLink").ClickAsync();
+        await Page.GetByTestId("tableauNavLink").ClickAsync();
         await Expect(Page).ToHaveTitleAsync("Tableau Home");
         await Page.GetByRole(AriaRole.Link, new() { Name = "Create New" }).ClickAsync();
         await Expect(Page).ToHaveTitleAsync("Tableau Add");
