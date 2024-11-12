e936cc5c4c0735432225a3b6064a3be68271bee8
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-12 00:08:00 -0700

    gawk -i inplace "{ gsub(/\/\/ ActualPropertyPlaceholder/, \"public string Name { get; set; } = string.Empty;\n\ \ \ \ // ActualPropertyPlaceholder\") }; { print }" Intrepion.KlondikeSolitaire.BusinessLogic/Entities/PileType.cs

--- Intrepion.KlondikeSolitaire.BusinessLogic/Entities/PileType.cs
+++ Intrepion.KlondikeSolitaire.BusinessLogic/Entities/PileType.cs
@@ -7,5 +7,6 @@ public class PileType
     public ApplicationUser? ApplicationUserUpdatedBy { get; set; }
     public Guid Id { get; set; }
 
+    public string Name { get; set; } = string.Empty;
     // ActualPropertyPlaceholder
 }
