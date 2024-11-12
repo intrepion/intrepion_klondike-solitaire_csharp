3ee489fdd589c2612721256f3453e223a6bbe219
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-12 00:05:17 -0700

    gawk -i inplace "{ gsub(/\/\/ ActualPropertyPlaceholder/, \"public ICollection<Foundation> Foundations { get; set; } = [];\n\ \ \ \ // ActualPropertyPlaceholder\") }; { print }" Intrepion.KlondikeSolitaire.BusinessLogic/Entities/Game.cs

--- Intrepion.KlondikeSolitaire.BusinessLogic/Entities/Game.cs
+++ Intrepion.KlondikeSolitaire.BusinessLogic/Entities/Game.cs
@@ -14,5 +14,6 @@ public class Game
     public Status? StatusId { get; set; }
     public ICollection<CardStock> CardStocks { get; set; } = [];
     public ICollection<CardWaste> CardWastes { get; set; } = [];
+    public ICollection<Foundation> Foundations { get; set; } = [];
     // ActualPropertyPlaceholder
 }
