1fdd3546bb9acfccf83fb25ad6e09ca36c1edd65
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-12 00:05:55 -0700

    gawk -i inplace "{ gsub(/EntityNamePlaceholder/, \"Move\") }; { print }" Intrepion.KlondikeSolitaire.BusinessLogic/Grid/Admin/MoveGrid/MoveGridControls.cs

--- Intrepion.KlondikeSolitaire.BusinessLogic/Grid/Admin/MoveGrid/MoveGridControls.cs
+++ Intrepion.KlondikeSolitaire.BusinessLogic/Grid/Admin/MoveGrid/MoveGridControls.cs
@@ -1,7 +1,7 @@
-namespace Intrepion.KlondikeSolitaire.BusinessLogic.Grid.Admin.EntityNamePlaceholderGrid;
+namespace Intrepion.KlondikeSolitaire.BusinessLogic.Grid.Admin.MoveGrid;
 
 // State of grid filters.
-public class EntityNamePlaceholderGridControls(IPageHelper pageHelper) : IEntityNamePlaceholderFilters
+public class MoveGridControls(IPageHelper pageHelper) : IMoveFilters
 {
     // Keep state of paging.
     public IPageHelper PageHelper { get; set; } = pageHelper;
@@ -10,13 +10,13 @@ public class EntityNamePlaceholderGridControls(IPageHelper pageHelper) : IEntity
     public bool Loading { get; set; }
 
     // Column to sort by.
-    public EntityNamePlaceholderFilterColumns SortColumn { get; set; } = EntityNamePlaceholderFilterColumns.Id;
+    public MoveFilterColumns SortColumn { get; set; } = MoveFilterColumns.Id;
 
     // True when sorting ascending, otherwise sort descending.
     public bool SortAscending { get; set; } = true;
 
     // Column filtered text is against.
-    public EntityNamePlaceholderFilterColumns FilterColumn { get; set; } = EntityNamePlaceholderFilterColumns.Id;
+    public MoveFilterColumns FilterColumn { get; set; } = MoveFilterColumns.Id;
 
     // Text to filter on.
     public string? FilterText { get; set; }
