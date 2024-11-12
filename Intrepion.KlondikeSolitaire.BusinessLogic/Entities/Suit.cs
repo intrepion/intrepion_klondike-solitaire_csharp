f9dd9b9d0ad9afd1212490c5335733de6317cbd3
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-12 00:13:39 -0700

    gawk -i inplace "{ gsub(/\/\/ ActualPropertyPlaceholder/, \"public string Name { get; set; } = string.Empty;\n\ \ \ \ // ActualPropertyPlaceholder\") }; { print }" Intrepion.KlondikeSolitaire.BusinessLogic/Entities/Suit.cs

--- Intrepion.KlondikeSolitaire.BusinessLogic/Entities/Suit.cs
+++ Intrepion.KlondikeSolitaire.BusinessLogic/Entities/Suit.cs
@@ -8,5 +8,6 @@ public class Suit
     public Guid Id { get; set; }
 
     public string Code { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
     // ActualPropertyPlaceholder
 }
